13cd817fc8bd66c21485d3fa7948c0c29787de8a
mohsen <hasanimohammadhosein@gmail.com>
2021-11-28 23:38:17 +0330

    multiple with args

--- 1.py
+++ 1.py
@@ -1,3 +1,8 @@
+def f(*n):
+    total = 1
+    for i in n:
+        total *= i
+    return total
 x = 3
 y = [1, 2, 3, 4, 5]
 print(id(y))
@@ -6,3 +11,4 @@ for i in range(x):
     print(i)
     if i == 2:
         break
+print(f(1,4,2,6))
