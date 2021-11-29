a90cf48f626998c74094dbb601b762e39bd7fd1c
mohsen <hasanimohammadhosein@gmail.com>
2021-11-29 10:24:02 +0330

    sort with key and function

--- 1.py
+++ 1.py
@@ -6,7 +6,7 @@ def f(*n):
 
 
 x = 3
-y = [1, 2, 3, 4, 6, 7]
+y = [[1, 3], [13, 14], [4, 4], [15, 1], [99, 80], [4, 13]]
 print(id(y))
 print(2)
 for i in range(x):
@@ -16,4 +16,13 @@ for i in range(x):
 print(f(1, 4, 2, 6))
 s = "hello world"
 s = "H"
+# del y[0:3]
+
+
+def sort_item(item):
+    return item[1]
+
+
+y.sort(key=sort_item)
+print(y)
 print(s[0])
